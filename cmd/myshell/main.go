ab6fbd88c6e403c900af6ddc25538354b8023951
aayushxrj <aayush.ranjan0904@gmail.com>
2024-06-07 23:31:52 +0530

    REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,13 +7,16 @@ import (
 )
 
 func main() {
-	fmt.Fprintf(os.Stdout, "$ ")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprintf(os.Stdout, "$ ")
 
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		os.Exit(0)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			os.Exit(0)
+		}
+
+		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
 	}
-
-	fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
 }
